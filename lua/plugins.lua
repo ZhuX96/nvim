7dfc0d3d1b256d9852c919052794561352866123
Zhu Xu <xuzhu1996@gmail.com>
2023-10-13 12:00:28 -0700

    upgrade telescope.nvim from 0.1.0 to 0.1.4

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -55,7 +55,7 @@ return require('packer').startup(function(use)
 
   use ({ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' })
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
