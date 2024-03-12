20cc70a9dcb3c358d728028312581951a02ef2cb
Zhu Xu <xuzhu1996@gmail.com>
2024-03-11 22:14:38 -0700

    enable transparent

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -8,6 +8,7 @@ vim.api.nvim_create_autocmd('TextYankPost',{
 })
 
 vim.g.everforest_diagnostic_line_highlight = 1
-vim.g.everforest_background = 'hard'
+-- vim.g.everforest_background = 'hard'
+vim.g.everforest_transparent_background = 2
 vim.cmd('colorscheme everforest')
 
