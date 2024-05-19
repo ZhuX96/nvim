f9a3ceca4b53fcd504762bc58d38e8e3cd4338f7
Zhu Xu <xuzhu1996@gmail.com>
2024-05-18 20:29:40 -0700

    modify nvim-tree icon

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -32,13 +32,13 @@ return require('packer').startup(function(use)
   use 'lewis6991/gitsigns.nvim'
   use {'akinsho/toggleterm.nvim', tag = '*'}
   use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    'nvim-tree/nvim-tree.lua',
+    requires = { 'nvim-tree/nvim-web-devicons' },
   }
 
   use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {'nvim-tree/nvim-web-devicons'}
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
 
   -- remove nvim-macos/lib/nvim/parser
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,6 +1,12 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local nvim_tree_web_devicons_status, nvim_tree_web_devicons = pcall(require, 'nvim-web-devicons')
+if not nvim_tree_web_devicons_status then
+  print('Failed to require nvim-web-devicons')
+  return
+end
+
 local nvim_tree_status, nvim_tree = pcall(require, 'nvim-tree')
 if not nvim_tree_status then
   print('Failed to require nvim-tree')
@@ -10,10 +16,20 @@ end
 nvim_tree.setup({
   renderer = {
     icons = {
+      web_devicons = {
+        file = {
+          enable = true,
+          color = true,
+        },
+        folder = {
+          enable = false,
+          color = true,
+        },
+      },
       glyphs = {
         folder = {
-          arrow_closed = "", -- arrow when folder is closed
-          arrow_open = "", -- arrow when folder is open
+          arrow_closed = "⏵", -- arrow when folder is closed
+          arrow_open = "⏷", -- arrow when folder is open
         },
       },
     },
