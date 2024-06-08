6ea1407877e513facacf3f9fc13c257eeed1b7c4
Zhu Xu <xuzhu1996@gmail.com>
2024-06-07 23:04:43 -0700

    incremental selection

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -15,5 +15,14 @@ treesitter.setup {
   },
   auto_tag = {
     enable = true
-  }
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<CR>',
+      node_incremental = '<CR>',
+      scope_incremental = false,
+      node_decremental = '<bs>',
+    },
+  },
 }
