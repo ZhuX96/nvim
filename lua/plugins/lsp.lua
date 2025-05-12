8a9475825ffc2e9c74f9abab742788c548e82fae
Zhu Xu <xuzhu1996@gmail.com>
2025-05-11 18:54:55 -0700

    update lsp & lsp plugins & dashboard

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,7 +5,7 @@ return {
   event = "LazyFile",
   dependencies = {
     "mason.nvim",
-    { "williamboman/mason-lspconfig.nvim", opts = { ensure_installed = { "lua_ls", "clangd" } } },
+    "williamboman/mason-lspconfig.nvim",
   },
   opts = function()
     ---@class PluginLspOpts
@@ -56,7 +56,6 @@ return {
         },
       },
       -- LSP Server Settings
-      ---@type lspconfig.options
       servers = {
         lua_ls = {
           -- mason = false, -- set to false if you don't want this server to be installed with mason
@@ -102,17 +101,25 @@ return {
             "--pch-storage=disk",
           },
         },
+
+        ["rust_analyzer"] = {
+          imports = {
+            granularity = {
+              group = "module",
+            },
+            prefix = "self",
+          },
+          cargo = {
+            buildScripts = {
+              enable = true,
+            },
+          },
+          procMacro = {
+            enable = true,
+          },
+        },
       },
-      -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
       setup = {
-        -- example to setup with typescript.nvim
-        -- tsserver = function(_, opts)
-        --   require("typescript").setup({ server = opts })
-        --   return true
-        -- end,
-        -- Specify * to use this function as a fallback for any server
         ["*"] = function(server, opts) end,
       },
     }
@@ -238,11 +245,8 @@ return {
 
     if have_mason then
       mlsp.setup({
-        ensure_installed = vim.tbl_deep_extend(
-          "force",
-          ensure_installed,
-          LazyVim.opts("mason-lspconfig.nvim").ensure_installed or {}
-        ),
+        ensure_installed = { "lua_ls", "clangd", "rust_analyzer" },
+        automatic_installation = false,
         handlers = { setup },
       })
     end
